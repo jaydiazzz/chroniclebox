164741d39ce3d49ed119e657c6276d00f921ae1b
jaydiazzz <jaydiazzz10@gmail.com>
2017-03-13 12:44:40 -0700

    Create index.js

---
+++ Javascript/index.js
@@ -0,0 +1,23 @@
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $(".nav");
+	  var $header = $("header");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $header.height());
+	});
+});
+
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $(".bar1,.bar2,.bar3");
+	  var $header = $("header");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $header.height());
+	});
+});
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+$('.container').on('click', function() {
+    $('.nav').toggleClass('open', 300, 'easeOutQuad');
+});
