ab222c1d769eb441667141c9eba74bdae58f774b
jaydiazzz <jaydiazzz10@gmail.com>
2017-04-21 11:34:51 -0700

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,15 +1,1 @@
-$(function () {
-  $(document).scroll(function () {
-	  var $nav = $(".nav");
-	  var $header = $("header");
-	  $nav.toggleClass('scrolled', $(this).scrollTop() > $header.height());
-	});
-});
-
-$(function () {
-  $(document).scroll(function () {
-	  var $nav = $(".bar1,.bar2,.bar3");
-	  var $header = $("header");
-	  $nav.toggleClass('scrolled', $(this).scrollTop() > $header.height());
-	});
-});
+$(function(){$(document).scroll(function(){var $nav=$(".nav");var $header=$("header");$nav.toggleClass('scrolled',$(this).scrollTop()>$header.height())})});$(function(){$(document).scroll(function(){var $nav=$(".bar1,.bar2,.bar3");var $header=$("header");$nav.toggleClass('scrolled',$(this).scrollTop()>$header.height())})})
