bb26b293e26ea6c7c150b69718b35f0f634d22a8
jaydiazzz <jaydiazzz10@gmail.com>
2017-04-21 11:35:16 -0700

    Update main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,7 +1,2 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-}
-
-$('.container').on('click', function() {
-    $('.nav').toggleClass('open', 300, 'easeOutQuad');
-});
+function myFunction(x){x.classList.toggle("change")}
+$('.container').on('click',function(){$('.nav').toggleClass('open',300,'easeOutQuad')})
