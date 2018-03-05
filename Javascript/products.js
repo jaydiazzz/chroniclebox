839d08306c4aaa79d9a037392c41cb20d99f1316
Jay <jaydiazzz10@gmail.com>
2018-03-05 15:31:25 -0800

    testing

--- Javascript/products.js
+++ Javascript/products.js
@@ -5,26 +5,34 @@ function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
+        slides[i].style.display = "none";
     }
     slideIndex++;
-    
-    if (slideIndex > slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
+
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 10000); // Change image every 2 seconds
-    
-    if($(".first").css('display') == 'block'){
-      $('.1, .2, .3').removeClass('active');
-      $('.1').addClass('active');
-    } else if($(".second").css('display') == 'block'){
-      $('.1, .2, .3').removeClass('active');
-      $('.2').addClass('active');
-    } else if($(".third").css('display') == 'block'){
-      $('.1, .2, .3').removeClass('active');
-      $('.3').addClass('active');
+    var one = document.getElementsByClassName("1");
+    var two = document.getElementsByClassName("2");
+    var three = document.getElementsByClassName("3");
+    if(document.querySelector(".first").style.display == "block"){
+      one.classList.remove("active");
+      two.classList.remove("active");
+      three.classList.remove("active");
+      one.classList.add('active');
+    } else if(document.querySelector(".second").style.display == 'block'){
+      one.classList.remove("active");
+      two.classList.remove("active");
+      three.classList.remove("active");
+      two.classList.add('active');
+    } else if(document.querySelector(".third").style.display == 'block'){
+      one.classList.remove("active");
+      two.classList.remove("active");
+      three.classList.remove("active");
+      three.classList.add('active');
     }
-    
+
     else{
-      $('.dot').removeClass('active')
+      document.querySelector('.dot').classList.remove('active');
     }
 };
